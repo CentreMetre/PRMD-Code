efc33a04cada7d364d4124025eb29d9c7c2c28dc
centremetre <luckyminer01@gmail.com>
2025-03-05 22:40:16 +0000

    Add initial code for testing

--- strain-gauge.py
+++ strain-gauge.py
@@ -2,4 +2,30 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 import time
 
+DT_PIN = 40 #gpio 21 https://pinout.xyz/pinout/5v_power
+SCK_PIN = 11 #gpio 17
 
+hx = HX711(DT_PIN, SCK_PIN)
+
+hx.set_reading_format("MSB", "MSB")
+
+hx.set_reference_unit(1)
+
+hx.reset()
+hx.tare()
+
+print("Scale is ready. Place and object to measure weight")
+
+try:
+    while True:
+        weight = hs.get_weight(5)
+        print(f"Weight: {weight:.2f} grams")
+
+        hx.power_down()
+        hx.power_up()
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Exiting")
+finally:
+    GPIO.cleanup()
