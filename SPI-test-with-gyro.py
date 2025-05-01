19b21fbb494b75560a478159aa8d0457c62297cc
centremetre <luckyminer01@gmail.com>
2025-05-01 13:53:39 +0100

    Add test

--- SPI-test-with-gyro.py
+++ SPI-test-with-gyro.py
@@ -1,18 +1,22 @@
 import spidev
 import time
 
+# Create an SPI object
 spi = spidev.SpiDev()
-spi.open(0, 0)  # Bus 0, Device 0 (CE0)
-spi.max_speed_hz = 50000
 
-def read_data():
-    data = spi.xfer2([0x01, 0x80, 0x00])  # Example SPI transfer (modify according to your sensor)
-    return data
+# Open SPI bus (0 is the default bus, 0 is the chip select)
+spi.open(0, 0)
 
-try:
-    while True:
-        print(read_data())
-        time.sleep(1)
+# Set SPI speed (in Hz), mode (0 or 3), and bit length
+spi.max_speed_hz = 50000  # Change this as needed
+spi.mode = 0  # SPI mode (0 to 3, depending on your device)
+spi.bits_per_word = 8  # 8 bits per word, most common
 
-finally:
+while True:
+    # Read data from SPI (example: reading 3 bytes)
+    data = spi.readbytes(3)
+
+    # Print the received data
+    print("Received Data:", data)
+except KeyboardInterrupt:
     spi.close()
