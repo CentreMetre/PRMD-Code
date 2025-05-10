f1aca6b542636e2e5b91c21cb81ea5bd831b31ee
CentreMetre <luckyminer01@gmail.com>
2025-05-11 00:44:13 +0100

    Add json function

--- utils/prmd_json.py
+++ utils/prmd_json.py
@@ -3,3 +3,7 @@ import json
 """
 Used to convert a session of data into a json file for uploading to the cloud.
 """
+
+def convert_reading_list_to_json(reading):
+    json_readings = json.dumps(reading, indent=4)
+    return json_readings
