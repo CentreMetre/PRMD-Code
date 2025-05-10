79502f07311db7518e827e9be4f10355902baca6
CentreMetre <luckyminer01@gmail.com>
2025-05-11 00:57:46 +0100

    add enum and sensor type in json

--- utils/prmd_json.py
+++ utils/prmd_json.py
@@ -1,9 +1,21 @@
 import json
+import sensor_enum
 
 """
 Used to convert a session of data into a json file for uploading to the cloud.
 """
 
-def convert_reading_list_to_json(reading):
-    json_readings = json.dumps(reading, indent=4)
+
+def convert_reading_list_to_json(readings, sensor_type: sensor_enum):
+    """
+
+    Args:
+        readings: The reading data
+        sensor_type: The type of sensor used, as a sensor_enum
+
+    Returns:
+
+    """
+    readings["sensor_type"] = sensor_type.value
+    json_readings = json.dumps(readings, indent=4)
     return json_readings
