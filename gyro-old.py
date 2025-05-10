aab3951b2f30e28a70e4efcdd3a936b698a22f2a
CentreMetre <luckyminer01@gmail.com>
2025-05-10 12:28:27 +0100

    Start calibration code

--- gyro-old.py
+++ gyro-old.py
@@ -1,15 +1,15 @@
 import smbus
 import time
 
-I2C_ADDR = 0x6A
+I2C_ADDR = 0x6A # Upper sensor
 bus = smbus.SMBus(1) # i2c bus 1
 
-#register addresses
+# register addresses
 WHO_AM_I = 0x0F
 CTRL1_XL = 0x10
 CTRL2_G = 0x11
-OUTX_L_G = 0x22 #gryo low byte
-OUTX_L_A = 0x28 #accelerator low bye
+OUTX_L_G = 0x22  # gryo low byte
+OUTX_L_A = 0x28  # accelerator low bye
 
 WHO_AM_I = 0x0F
 CTRL1_XL = 0x10
@@ -39,18 +39,82 @@ def read_data(register):
         value -= 65536  # Convert to signed value
     return value
 
-while True:
-    # Accelerometer readings
-    accel_x = read_data(OUTX_L_A)
-    accel_y = read_data(OUTY_L_A)
-    accel_z = read_data(OUTZ_L_A)
+calibration_readings = {
+    'accel': {'X': [], 'Y': [], 'Z': []},
+    'gyro': {'X': [], 'Y': [], 'Z': []}
+}
 
-    # Gyroscope readings
-    gyro_x = read_data(OUTX_L_G)
-    gyro_y = read_data(OUTY_L_G)
-    gyro_z = read_data(OUTZ_L_G)
+# while True:
+#     # Accelerometer readings
+#     accel_x = read_data(OUTX_L_A)
+#     accel_y = read_data(OUTY_L_A)
+#     accel_z = read_data(OUTZ_L_A)
+#
+#     # Gyroscope readings
+#     gyro_x = read_data(OUTX_L_G)
+#     gyro_y = read_data(OUTY_L_G)
+#     gyro_z = read_data(OUTZ_L_G)
+#
+#     print(f"Accel X: {accel_x}, Y: {accel_y}, Z: {accel_z}")
+#     print(f"Gyro  X: {gyro_x}, Y: {gyro_y}, Z: {gyro_z}")
+#     print()
+#     time.sleep(0.1)
 
-    print(f"Accel X: {accel_x}, Y: {accel_y}, Z: {accel_z}")
-    print(f"Gyro  X: {gyro_x}, Y: {gyro_y}, Z: {gyro_z}")
-    print()
-    time.sleep(0.1)
+
+def calibrate_final_data(data):
+    accel_bias_X_mean = calculate_mean(calibration_readings['accel']['X'])
+    accel_bias_Y_mean = calculate_mean(calibration_readings['accel']['Y'])
+    accel_bias_Z_mean = calculate_mean(calibration_readings['accel']['Z'])
+
+    gyro_bias_X_mean = calculate_mean(calibration_readings['gyro']['X'])
+    gyro_bias_Y_mean = calculate_mean(calibration_readings['gyro']['Y'])
+    gyro_bias_Z_mean = calculate_mean(calibration_readings['gyro']['Z'])
+
+
+
+def get_calibration_data():
+    calibration_readings = read_sensors_for_time(2)
+
+
+def read_sensors_for_time(seconds):
+    start = time.time()
+    end = start + seconds
+
+    readings = {
+        'accel': {'X': [], 'Y': [], 'Z': []},
+        'gyro': {'X': [], 'Y': [], 'Z': []}
+    }
+
+    while True:
+        # Accelerometer readings
+        accel_x = read_data(OUTX_L_A)
+        readings['accel']['X'].append(accel_x)
+
+        accel_y = read_data(OUTY_L_A)
+        readings['accel']['Y'].append(accel_y)
+
+        accel_z = read_data(OUTZ_L_A)
+        readings['accel']['Z'].append(accel_z)
+
+        # Gyroscope readings
+        gyro_x = read_data(OUTX_L_G)
+        readings['gyro']['X'].append(gyro_x)
+
+        gyro_y = read_data(OUTY_L_G)
+        readings['gyro']['Y'].append(gyro_y)
+
+        gyro_z = read_data(OUTZ_L_G)
+        readings['gyro']['Z'].append(gyro_z)
+
+        print(f"Accel X: {accel_x}, Y: {accel_y}, Z: {accel_z}")
+        print(f"Gyro  X: {gyro_x}, Y: {gyro_y}, Z: {gyro_z}")
+
+        if time.time() >= end:
+            return readings
+
+def calculate_mean(list):
+    length = 0
+    total_sum = 0
+    for i in range(len(list)):
+        length =+ 1
+        total_sum = total_sum + list[i]
