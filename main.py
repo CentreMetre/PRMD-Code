923c384a583050e28826a691d12b86749a5d4472
RhysKemp <rhyskemp15@gmail.com>
2025-05-11 15:51:38 +0100

    Refactor: Implement main function to load environment variables and initialize IoT Hub client Add: Create telemetry data JSON file for sensor readings

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from dotenv import load_dotenv
+from communication.client import get_iot_hub_client
 from communication.upload import run_upload
+import os
 
 
 def main():
-    pass
+    load_dotenv()
+    connection_string = os.getenv("IOT_CONNECTION_STRING")
+    client = get_iot_hub_client(connection_string)
+    run_upload(client=client)
 
 
 if __name__ == "__main__":
