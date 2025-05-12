3f36c1ea85299990df5df53e3dfd29e322ae7802
CentreMetre <luckyminer01@gmail.com>
2025-05-12 16:12:58 +0100

    add unix to iso timestamp function

--- utils/file_io.py
+++ utils/file_io.py
@@ -1,5 +1,5 @@
 import json
-import os
+from datetime import datetime
 
 
 def read_file(file_path: str) -> str:
@@ -45,7 +45,15 @@ def write_to_json_file(file_path: str, data: dict) -> None:
             f"An error occurred while writing to the file {file_path}. Session data may not be saved."
         ) from e
 
-    print(full_path)
 
-    with open(full_path, 'w') as f:
-        json.dump(readings, f, indent=4)
+def unix_to_iso(unix_time: float) -> str:
+    """
+
+    Args:
+        unix_time: The unix time to convert into ISO format.
+
+    Returns: The unix time given in an iso format
+
+    """
+    date_time = datetime.fromtimestamp(unix_time)
+    return date_time.isoformat()
