33232ec0c39370c444117051c77180bc82a3685c
CentreMetre <luckyminer01@gmail.com>
2025-05-12 17:08:06 +0100

    add code

--- strain-gauge.py
+++ strain-gauge.py
@@ -1,7 +1,21 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+DT = 5 #Logical GPIO pin 5 for DT (physical pin 29)
+SCK = 6 #Logical GPIO pin 6 for DT (physical pin 31)
+
+hx = HX711(dout_pin=DT, pd_sck_pin=SCK) # Initialise the HX711 with the pins needed
 
 GPIO.setmode(GPIO.BCM)
 
 def read_sensor_for_time_with_interval(time: int, interval: int):
     print("TO IMPLEMENT")
+
+def read_sensor():
+    weight = hx.get_weight(5)
+    print(f"WEIGHT: {weight}")
+    return weight
+
+if __name__ == "__main__":
+    print(read_sensor())
